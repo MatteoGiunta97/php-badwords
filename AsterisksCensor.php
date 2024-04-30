1a959e57d2d37244cd3e70d10c21a113b4f1bbe0
MatteoGiunta97 <matteogiunta@outlook.it>
2024-04-30 15:04:55 +0200

    feat: add replace function

--- AsterisksCensor.php
+++ AsterisksCensor.php
@@ -11,10 +11,17 @@ $badWord = $_GET['badWord'];
     <title>CENSOR</title>
 </head>
 <body>
+    <!-- Stampo il paragrafo e la sua lunghezza -->
     <p>
         <h1>Paragraph:</h1>
         <?php echo $paragraph; ?>
         <h2>Length: <?php echo strlen($paragraph); ?> </h2>
+    </p>
+    <hr>
+    <!-- Sostituisco la parola da censurare con asterischi -->
+    <p>
+        <h1>Censured Paragraph:</h1>
+        <?php echo str_replace($badWord, '***', $paragraph); ?>
     </p>
 </body>
 </html>
